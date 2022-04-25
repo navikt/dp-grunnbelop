f0b301fea096177bef0ef560b951e0aff241899e
Giao The Cung <giaothe@gmail.com>
2022-04-25 08:30:19 +0200

    Endre på G for gjusterings test

--- src/main/kotlin/no/nav/dagpenger/grunnbelop/Grunnbelop.kt
+++ src/main/kotlin/no/nav/dagpenger/grunnbelop/Grunnbelop.kt
@@ -7,7 +7,7 @@ import java.time.Month
 import java.time.YearMonth
 
 enum class Grunnbeløp(val verdi: BigDecimal, val iverksattFom: LocalDate) {
-    GjusteringsTest(verdi = 106399.toBigDecimal(), iverksattFom = LocalDate.now().plusYears(10)),
+    GjusteringsTest(verdi = 111000.toBigDecimal(), iverksattFom = LocalDate.of(2022, Month.MAY, 1)),
     FastsattI2021(verdi = 106399.toBigDecimal(), iverksattFom = LocalDate.of(2021, Month.MAY, 23)),
     FastsattI2020(verdi = 101351.toBigDecimal(), iverksattFom = LocalDate.of(2020, Month.SEPTEMBER, 19)),
     FastsattI2019(verdi = 99858.toBigDecimal(), iverksattFom = LocalDate.of(2019, Month.MAY, 26)),
